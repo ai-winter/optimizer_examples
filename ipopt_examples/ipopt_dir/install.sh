8f3ce11bca36f215c322a68ae1d7b2a1545d9efb
mr_winter <913982779@qq.com>
2024-06-24 21:27:59 +0800

    update: ipopt example

--- ipopt_examples/ipopt_dir/install.sh
+++ ipopt_examples/ipopt_dir/install.sh
@@ -13,3 +13,36 @@ cd ..
 # HSL
 git clone https://github.com/coin-or-tools/ThirdParty-HSL.git
 cd ThirdParty-HSL
+cp ../coinhsl.zip . && unzip coinhsl.zip
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+
+# MUMPS
+git clone https://github.com/coin-or-tools/ThirdParty-Mumps.git
+cd ThirdParty-Mumps
+sudo ./get.Mumps
+sudo ./configure
+sudo make
+sudo make install
+cd ..
+
+# ipopt
+git clone https://github.com/coin-or/Ipopt.git
+cd Ipopt
+mkdir build
+cd build
+sudo ../configure
+sudo make
+sudo make test
+sudo make install
+
+cd /usr/local/include
+sudo cp coin-or coin -r
+sudo ln -s /usr/local/lib/libcoinmumps.so.3 /usr/lib/libcoinmumps.so.3
+sudo ln -s /usr/local/lib/libcoinhsl.so.2 /usr/lib/libcoinhsl.so.2
+sudo ln -s /usr/local/lib/libipopt.so.3 /usr/lib/libipopt.so.3
+
+# cppad
+sudo apt install cppad
